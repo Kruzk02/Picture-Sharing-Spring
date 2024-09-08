7b1002c619494a6388a3fff9c9ca4a000993572c
thienphuc <thienphuc484@gmail.com>
2024-09-08 13:30:09 +0700

    Add ErrorResponse and GlobalExceptionHandler

---
+++ src/main/java/com/app/exception/ErrorResponse.java
@@ -0,0 +1,18 @@
+package com.app.exception;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class ErrorResponse {
+    private String message;
+    private int httpStatus;
+    private LocalDateTime timestamp;
+}
---
+++ src/main/java/com/app/exception/GlobalExceptionHandler.java
@@ -0,0 +1,21 @@
+package com.app.exception;
+
+import com.app.exception.sub.UserNotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import java.time.LocalDateTime;
+
+@ControllerAdvice
+public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
+
+    @ExceptionHandler(UserNotFoundException.class)
+    public ResponseEntity<ErrorResponse> handleUserNotFoundException(UserNotFoundException ex) {
+        ErrorResponse response = new ErrorResponse(ex.getMessage(), HttpStatus.NOT_FOUND.value(), LocalDateTime.now());
+        return new ResponseEntity<>(response, HttpStatus.NOT_FOUND);
+    }
+
+}
