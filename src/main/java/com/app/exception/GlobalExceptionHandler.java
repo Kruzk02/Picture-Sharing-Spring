46234c0fd79a54817245530f7e860323b80b8cec
thienphuc <thienphuc484@gmail.com>
2024-09-12 11:35:58 +0700

    Update GlobalExceptionHandler
    Handle BoardNotFoundException

--- src/main/java/com/app/exception/GlobalExceptionHandler.java
+++ src/main/java/com/app/exception/GlobalExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.app.exception;
 
+import com.app.exception.sub.BoardNotFoundException;
 import com.app.exception.sub.CommentNotFoundException;
 import com.app.exception.sub.UserNotFoundException;
 import org.springframework.http.HttpStatus;
@@ -21,6 +22,12 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
 
     @ExceptionHandler(CommentNotFoundException.class)
     public ResponseEntity<ErrorResponse> handleCommentNotFoundException(CommentNotFoundException ex) {
+        ErrorResponse response = new ErrorResponse(ex.getMessage(), HttpStatus.NOT_FOUND.value(), LocalDateTime.now());
+        return new ResponseEntity<>(response, HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(BoardNotFoundException.class)
+    public ResponseEntity<ErrorResponse> handleBoardNotFoundException(BoardNotFoundException ex) {
         ErrorResponse response = new ErrorResponse(ex.getMessage(), HttpStatus.NOT_FOUND.value(), LocalDateTime.now());
         return new ResponseEntity<>(response, HttpStatus.NOT_FOUND);
     }
