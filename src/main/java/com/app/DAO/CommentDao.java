42c01754485a47ddb9c6e153e6138b02fa903588
thienphuc <thienphuc484@gmail.com>
2024-09-12 09:59:54 +0700

    Update commentDao

--- src/main/java/com/app/DAO/CommentDao.java
+++ src/main/java/com/app/DAO/CommentDao.java
@@ -2,9 +2,12 @@ package com.app.DAO;
 
 import com.app.Model.Comment;
 
+import java.util.List;
+
 public interface CommentDao {
 
     Comment save(Comment comment);
     Comment findById(Long id);
+    List<Comment> findByPinId(Long pinId);
     int deleteById(Long id);
 }
--- src/main/java/com/app/DAO/Impl/CommentDaoImpl.java
+++ src/main/java/com/app/DAO/Impl/CommentDaoImpl.java
@@ -1,7 +1,10 @@
 package com.app.DAO.Impl;
 
 import com.app.DAO.CommentDao;
+import com.app.DAO.PinDao;
+import com.app.DAO.UserDao;
 import com.app.Model.Comment;
+import com.app.exception.sub.CommentNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
@@ -13,15 +16,20 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.List;
 
 @Repository
 public class CommentDaoImpl implements CommentDao {
 
     private final JdbcTemplate jdbcTemplate;
+    private final UserDao userDao;
+    private final PinDao pinDao;
 
     @Autowired
-    public CommentDaoImpl(JdbcTemplate jdbcTemplate) {
+    public CommentDaoImpl(JdbcTemplate jdbcTemplate, UserDao userDao, PinDao pinDao) {
         this.jdbcTemplate = jdbcTemplate;
+        this.userDao = userDao;
+        this.pinDao = pinDao;
     }
 
     @Override
@@ -53,9 +61,19 @@ public class CommentDaoImpl implements CommentDao {
     public Comment findById(Long id) {
         try{
             String sql = "SELECT * FROM comments WHERE id = ?";
-            return jdbcTemplate.queryForObject(sql,new CommentRowMapper(),id);
+            return jdbcTemplate.queryForObject(sql,new CommentRowMapper(userDao,pinDao),id);
         }catch (Exception e){
-            return null;
+            throw new CommentNotFoundException("Comment not found with a id: " + id);
+        }
+    }
+
+    @Override
+    public List<Comment> findByPinId(Long pinId) {
+        try{
+            String sql = "SELECT * FROM comments WHERE pin_id = ?";
+            return jdbcTemplate.query(sql, new CommentRowMapper(userDao, pinDao), pinId);
+        }catch (Exception e){
+            throw new CommentNotFoundException("Comment not found with a pin id: " + pinId);
         }
     }
 
@@ -65,19 +83,27 @@ public class CommentDaoImpl implements CommentDao {
             String sql = "DELETE FROM comments WHERE id = ?";
             return jdbcTemplate.update(sql,id);
         }catch (Exception e){
-            return 0;
+            throw new CommentNotFoundException("Comment not found with a id: " + id);
         }
     }
 }
 
 class CommentRowMapper implements RowMapper<Comment>{
+    private final UserDao userDao;
+    private final PinDao pinDao;
+
+    public CommentRowMapper(UserDao userDao, PinDao pinDao) {
+        this.userDao = userDao;
+        this.pinDao = pinDao;
+    }
 
     @Override
     public Comment mapRow(ResultSet rs, int rowNum) throws SQLException {
         Comment comment = new Comment();
         comment.setId(rs.getLong("id"));
         comment.setContent(rs.getString("content"));
-
+        comment.setUser(userDao.findUserById(rs.getLong("user_id")));
+        comment.setPin(pinDao.findById(rs.getLong("pin_id")));
         return comment;
     }
 }
