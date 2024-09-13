9114915cf1eb80aa1dfe03fe9b291e4b2fa914eb
thienphuc <thienphuc484@gmail.com>
2024-09-13 10:34:43 +0700

    Update GlobalExceptionHandler to handle PinNotFoundException

--- src/main/java/com/app/exception/GlobalExceptionHandler.java
+++ src/main/java/com/app/exception/GlobalExceptionHandler.java
@@ -2,6 +2,7 @@ package com.app.exception;
 
 import com.app.exception.sub.BoardNotFoundException;
 import com.app.exception.sub.CommentNotFoundException;
+import com.app.exception.sub.PinNotFoundException;
 import com.app.exception.sub.UserNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -28,6 +29,12 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
 
     @ExceptionHandler(BoardNotFoundException.class)
     public ResponseEntity<ErrorResponse> handleBoardNotFoundException(BoardNotFoundException ex) {
+        ErrorResponse response = new ErrorResponse(ex.getMessage(), HttpStatus.NOT_FOUND.value(), LocalDateTime.now());
+        return new ResponseEntity<>(response, HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(PinNotFoundException.class)
+    public ResponseEntity<ErrorResponse> handlePinNotFoundException(PinNotFoundException ex) {
         ErrorResponse response = new ErrorResponse(ex.getMessage(), HttpStatus.NOT_FOUND.value(), LocalDateTime.now());
         return new ResponseEntity<>(response, HttpStatus.NOT_FOUND);
     }
