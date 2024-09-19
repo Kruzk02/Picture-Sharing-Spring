7888a5ef092151e3c1cc6abb1e6b6acfd93ae276
thienphuc <thienphuc484@gmail.com>
2024-09-19 11:48:50 +0700

    Add SpringAsyncConfig
    Enable async for i/o operation method

---
+++ src/main/java/com/app/Config/SpringAsyncConfig.java
@@ -0,0 +1,36 @@
+package com.app.Config;
+
+import org.springframework.aop.interceptor.AsyncUncaughtExceptionHandler;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.AsyncConfigurer;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+import java.util.concurrent.Executor;
+
+@Configuration
+@EnableAsync
+public class SpringAsyncConfig implements AsyncConfigurer {
+
+    @Override
+    public Executor getAsyncExecutor() {
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setCorePoolSize(5);
+        executor.setMaxPoolSize(10);
+        executor.setQueueCapacity(100);
+        executor.setThreadNamePrefix("Async-");
+        executor.initialize();
+        return executor;
+    }
+
+    @Override
+    public AsyncUncaughtExceptionHandler getAsyncUncaughtExceptionHandler() {
+        return (ex, method, params) -> {
+            System.err.println("Exception message - " + ex.getMessage());
+            System.err.println("Method name - " + method.getName());
+            for (Object param : params) {
+                System.err.println("Parameter value - " + param);
+            }
+        };
+    }
+}
