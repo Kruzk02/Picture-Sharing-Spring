e6cd1e71f443eaa16a40bcd28d10f141b88407fb
thienphuc <thienphuc484@gmail.com>
2024-09-27 09:12:11 +0700

    Add rate limit

--- src/main/java/com/app/Config/RedisConfig.java
+++ src/main/java/com/app/Config/RedisConfig.java
@@ -1,5 +1,16 @@
 package com.app.Config;
 
+import io.github.bucket4j.Bandwidth;
+import io.github.bucket4j.BucketConfiguration;
+import io.github.bucket4j.distributed.ExpirationAfterWriteStrategy;
+import io.github.bucket4j.distributed.proxy.ProxyManager;
+import io.github.bucket4j.redis.lettuce.cas.LettuceBasedProxyManager;
+import io.lettuce.core.RedisClient;
+import io.lettuce.core.RedisURI;
+import io.lettuce.core.api.StatefulRedisConnection;
+import io.lettuce.core.codec.ByteArrayCodec;
+import io.lettuce.core.codec.RedisCodec;
+import io.lettuce.core.codec.StringCodec;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -9,6 +20,9 @@ import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 
+import java.time.Duration;
+import java.util.function.Supplier;
+
 @Configuration
 public class RedisConfig {
 
@@ -17,6 +31,23 @@ public class RedisConfig {
 
     @Value("${redis.port}")
     private int redisPort;
+
+    @Bean
+    public ProxyManager<String> lettuceBasedProxyManager() {
+        RedisClient redisClient = RedisClient.create(RedisURI.builder().withHost(redisHost).withPort(redisPort).withSsl(false).build());
+        StatefulRedisConnection<String,byte[]> redisConnection = redisClient.connect(RedisCodec.of(StringCodec.UTF8, ByteArrayCodec.INSTANCE));
+        return LettuceBasedProxyManager.builderFor(redisConnection).withExpirationStrategy(ExpirationAfterWriteStrategy.basedOnTimeForRefillingBucketUpToMax(Duration.ofMinutes(1L))).build();
+    }
+
+    @Bean
+    public Supplier<BucketConfiguration> bucketConfiguration() {
+        return () -> BucketConfiguration.builder()
+                .addLimit(Bandwidth.builder()
+                        .capacity(200L)
+                        .refillGreedy(200L,Duration.ofMinutes(1L))
+                        .build())
+                .build();
+    }
 
     @Bean
     public LettuceConnectionFactory redisConnectionFactory(){
