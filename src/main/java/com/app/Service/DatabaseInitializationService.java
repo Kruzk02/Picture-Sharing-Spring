757332fc43497bdad69811c9924e3b1f301e6420
thienphuc <thienphuc484@gmail.com>
2024-09-29 13:06:32 +0700

    Update DatabaseInitializationService.

--- src/main/java/com/app/Service/DatabaseInitializationService.java
+++ src/main/java/com/app/Service/DatabaseInitializationService.java
@@ -1,54 +1,83 @@
 package com.app.Service;
 
-import com.app.DAO.Impl.RoleDaoImpl;
+import com.app.DAO.PrivilegeDao;
+import com.app.DAO.RoleDao;
+import com.app.Model.Privilege;
 import com.app.Model.Role;
-import jakarta.annotation.PostConstruct;
+import com.app.Model.User;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.Resource;
+import org.springframework.jdbc.core.BatchPreparedStatementSetter;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.util.FileCopyUtils;
 
-import java.io.IOException;
 import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.*;
 
 @Service
 @Log4j2
-public class DatabaseInitializationService {
+public class DatabaseInitializationService implements ApplicationListener<ContextRefreshedEvent> {
 
     private final JdbcTemplate jdbcTemplate;
-    private final RoleDaoImpl roleDao;
+    private final RoleDao roleDao;
+    private final PrivilegeDao privilegeDao;
+    private final PasswordEncoder passwordEncoder;
+    private boolean alreadySetup = false;
 
     @Autowired
-    public DatabaseInitializationService(JdbcTemplate jdbcTemplate, RoleDaoImpl roleDao) {
+    public DatabaseInitializationService(JdbcTemplate jdbcTemplate, RoleDao roleDao, PrivilegeDao privilegeDao, PasswordEncoder passwordEncoder) {
         this.jdbcTemplate = jdbcTemplate;
         this.roleDao = roleDao;
+        this.privilegeDao = privilegeDao;
+        this.passwordEncoder = passwordEncoder;
     }
 
-    @PostConstruct
-    public void initializeDatabase() {
-        try {
-            executeSqlScript("user.sql");
-            executeSqlScript("board.sql");
-            executeSqlScript("pin.sql");
-            executeSqlScript("board_pin.sql");
-            executeSqlScript("roles.sql");
-            executeSqlScript("users_roles.sql");
-            executeSqlScript("comment.sql");
-            executeSqlScript("sub_comment.sql");
+    @Override
+    @Transactional
+    public void onApplicationEvent(ContextRefreshedEvent event) {
+        if (alreadySetup) return;
 
-            Role adminRole = createRoleIfNotFound("ADMIN");
-            Role userRole = createRoleIfNotFound("USER");
+        executeSqlScript("user.sql");
+        executeSqlScript("board.sql");
+        executeSqlScript("pin.sql");
+        executeSqlScript("board_pin.sql");
+        executeSqlScript("roles.sql");
+        executeSqlScript("privileges.sql");
+        executeSqlScript("roles_privileges.sql");
+        executeSqlScript("users_roles.sql");
+        executeSqlScript("comment.sql");
+        executeSqlScript("sub_comment.sql");
 
-        } catch (Exception e) {
-            log.error("Error initializing database: {}", e.getMessage());
-        }
+        createPrivilegeIfNotFound("READ_PRIVILEGE");
+        createPrivilegeIfNotFound("WRITE_PRIVILEGE");
+
+        createRoleIfNotFound("ROLE_ADMIN");
+        createRoleIfNotFound("ROLE_USER");
+
+        User user = User.builder()
+                .email("phucnguyen@gmail.com")
+                .username("phucnguyen")
+                .password(passwordEncoder.encode("123123"))
+                .build();
+        createAdminAccount(user);
+
+        alreadySetup = true;
     }
 
-    private void executeSqlScript(String fileName) throws IOException {
+    private void executeSqlScript(String fileName) {
         Resource resource = new ClassPathResource(fileName);
         try (InputStreamReader reader = new InputStreamReader(resource.getInputStream(), StandardCharsets.UTF_8)) {
             String sqlScript = FileCopyUtils.copyToString(reader);
@@ -59,19 +88,60 @@ public class DatabaseInitializationService {
         }
     }
 
-    private Role createRoleIfNotFound(String name){
+    private void createAdminAccount(User user) {
+
+        String userSql = "INSERT INTO users (username, email, password) VALUES (?, ?, ?)";
+        KeyHolder keyHolder = new GeneratedKeyHolder();
+        int rowsAffected = jdbcTemplate.update(connection -> {
+            PreparedStatement ps = connection.prepareStatement(userSql, Statement.RETURN_GENERATED_KEYS);
+            ps.setString(1, user.getUsername());
+            ps.setString(2, user.getEmail());
+            ps.setString(3, user.getPassword());
+            return ps;
+        }, keyHolder);
+
+        if (rowsAffected > 0) {
+            user.setId(Objects.requireNonNull(keyHolder.getKey()).longValue());
+
+            String userRoleSql = "INSERT INTO users_roles (user_id, role_id) VALUES (?, ?)";
+            jdbcTemplate.batchUpdate(userRoleSql, new BatchPreparedStatementSetter() {
+                @Override
+                public void setValues(PreparedStatement ps, int i) throws SQLException {
+                    ps.setLong(1, user.getId());
+                    ps.setInt(2, 1);
+                }
+
+                @Override
+                public int getBatchSize() {
+                    return 1;
+                }
+            });
+
+        }
+    }
+    private void createRoleIfNotFound(String name){
         log.info("Checking if role exist with a name: {}",name);
         Role role = roleDao.findByName(name);
-        if(role == null){
+        Optional<Role> roleOptional = Optional.ofNullable(role);
+        if(roleOptional.isEmpty()){
             log.info("Role with name {} not found. Create new Role",name);
-
             role = new Role(name);
             roleDao.create(role);
 
             log.info("Role created: {}",role);
-        }else{
-            log.info("Role with name {} already exist",name);
         }
-        return role;
     }
+
+    private void createPrivilegeIfNotFound(String name){
+        log.info("Checking if privilege exist with a name: {}",name);
+        Privilege privilege = privilegeDao.findByName(name);
+        Optional<Privilege> privilegeOptional = Optional.ofNullable(privilege);
+        if (privilegeOptional.isEmpty()) {
+            log.info("Privilege with name {} not found. Create new Privilege",name);
+            privilege = new Privilege(name);
+            privilegeDao.create(privilege);
+            log.info("Privilege create: {}",privilege);
+        }
+    }
+
 }
