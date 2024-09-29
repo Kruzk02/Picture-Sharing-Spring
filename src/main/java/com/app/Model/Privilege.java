330124a0769f15e96978cf4126e0d6a8ad9b59cf
thienphuc <thienphuc484@gmail.com>
2024-09-29 10:55:01 +0700

    Add privilege model class

---
+++ src/main/java/com/app/Model/Privilege.java
@@ -0,0 +1,19 @@
+package com.app.Model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Privilege {
+    private Long id;
+    private String name;
+
+    public Privilege(String name) {
+        this.name = name;
+    }
+}
