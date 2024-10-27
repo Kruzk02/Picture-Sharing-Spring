93832b6d253b5c6c68008f8a05a0fd8b1981741c
thienphuc <thienphuc484@gmail.com>
2024-10-27 11:02:21 +0700

    Update UserController

--- src/main/java/com/app/Controller/UserController.java
+++ src/main/java/com/app/Controller/UserController.java
@@ -6,6 +6,11 @@ import com.app.DTO.UpdateUserDTO;
 import com.app.Jwt.JwtProvider;
 import com.app.Model.User;
 import com.app.Service.UserService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -33,8 +38,21 @@ public class UserController {
         this.jwtProvider = jwtProvider;
     }
 
+    @Operation(summary = "Login account")
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "200", description = "Successfully Login",
+            content = @Content(mediaType = "application/json")),
+        @ApiResponse(responseCode = "400", description = "Invalid input"),
+        @ApiResponse(responseCode = "500", description = "Internal server error")
+    })
     @PostMapping("/login")
-    public ResponseEntity<Map<String,String>> login(@RequestBody LoginDTO loginDTO){
+    public ResponseEntity<Map<String,String>> login(
+        @io.swagger.v3.oas.annotations.parameters.RequestBody(
+            description = "Login Data",required = true,
+            content = @Content(mediaType = "application/json", schema = @Schema(implementation = LoginDTO.class))
+        )
+        @RequestBody LoginDTO loginDTO
+    ){
         User user = userService.login(loginDTO);
         Map<String,String> response = new HashMap<>();
 
@@ -45,6 +63,13 @@ public class UserController {
         return ResponseEntity.status(HttpStatus.OK).contentType(MediaType.APPLICATION_JSON).body(response);
     }
 
+    @Operation(summary = "Register user account")
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "201", description = "Success create account",content = {@Content(mediaType = "application/json",schema = @Schema(implementation = User.class))}),
+        @ApiResponse(responseCode = "400", description = "Invalid input", content = @Content(mediaType = "application/json")),
+        @ApiResponse(responseCode = "409", description = "Username or email is already taken", content = @Content(mediaType = "application/json")),
+        @ApiResponse(responseCode = "500", description = "Internal server error", content = @Content(mediaType = "application/json"))
+    })
     @PostMapping("/register")
     public ResponseEntity<Map<String, Object>> register(@RequestBody RegisterDTO registerDTO) {
         User existingEmail = userService.findUserByEmail(registerDTO.getEmail());
@@ -56,30 +81,32 @@ public class UserController {
             response.put("status", "error");
             response.put("message", "Email is already taken.");
             response.put("timestamp", String.valueOf(LocalDateTime.now()));
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(response);
+            return ResponseEntity.status(HttpStatus.CONFLICT).body(response);
         }
 
         if (existingUsername != null) {
             response.put("status", "error");
             response.put("message", "Username is already taken.");
             response.put("timestamp", String.valueOf(LocalDateTime.now()));
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(response);
+            return ResponseEntity.status(HttpStatus.CONFLICT).body(response);
         }
 
         userService.register(registerDTO);
         response.put("status", "ok");
         response.put("message", "successfully registered");
         response.put("timestamp", String.valueOf(LocalDateTime.now()));
-        return ResponseEntity.status(HttpStatus.OK).contentType(MediaType.APPLICATION_JSON).body(response);
+        return ResponseEntity.status(HttpStatus.CREATED).contentType(MediaType.APPLICATION_JSON).body(response);
     }
 
+    @Operation(summary = "Get username from token")
     @GetMapping("/get-username")
     public ResponseEntity<String> getUsernameFromToken(){
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
         String username = authentication.getName();
-        return ResponseEntity.ok(username);
+        return ResponseEntity.status(HttpStatus.OK).contentType(MediaType.APPLICATION_JSON).body(username);
     }
 
+    @Operation(summary = "Update user info")
     @PutMapping("/update-user-information")
     public ResponseEntity<?> updateUserInformation( @RequestBody UpdateUserDTO updateUserDTO){
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
