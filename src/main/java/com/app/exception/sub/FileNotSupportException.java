599162a3d69174e51ce7eb9b6fdbf826fa629853
thienphuc <thienphuc484@gmail.com>
2024-11-09 09:57:37 +0700

    Add FileNotSupportException for hanlding wrong type of file

---
+++ src/main/java/com/app/exception/sub/FileNotSupportException.java
@@ -0,0 +1,7 @@
+package com.app.exception.sub;
+
+public class FileNotSupportException extends RuntimeException {
+    public FileNotSupportException(String message) {
+        super(message);
+    }
+}
