109e4dc31ff53f7f2523a6b4f2c6e43ed04ad06e
Thien phuc <thienphuc484@gmail.com>
2024-12-19 11:01:04 +0700

    feat: add MediaDao

---
+++ src/main/java/com/app/DAO/Impl/MediaDaoImpl.java
@@ -0,0 +1,90 @@
+package com.app.DAO.Impl;
+
+import com.app.DAO.MediaDao;
+import com.app.Model.Media;
+import com.app.Model.MediaType;
+import com.app.exception.sub.MediaNotFoundException;
+import lombok.AllArgsConstructor;
+import org.springframework.dao.DataAccessException;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Isolation;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Objects;
+
+@Repository
+@AllArgsConstructor
+public class MediaDaoImpl implements MediaDao {
+
+    private final JdbcTemplate template;
+
+    @Transactional(propagation = Propagation.REQUIRED, isolation = Isolation.REPEATABLE_READ)
+    @Override
+    public Media save(Media media) {
+        try {
+            String sql = "INSERT INTO media(url, media_type) VALUES(?,?)";
+            KeyHolder keyHolder = new GeneratedKeyHolder();
+
+            int row = template.update(conn -> {
+                PreparedStatement ps = conn.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+                ps.setString(1, media.getUrl());
+                ps.setString(2, media.getMediaType().toString());
+                return ps;
+            }, keyHolder);
+
+            if (row > 0) {
+                media.setId(Objects.requireNonNull(keyHolder.getKey()).longValue());
+                return media;
+            } else {
+                return null;
+            }
+        } catch (Exception e) {
+            return null;
+        }
+    }
+
+    @Transactional(propagation = Propagation.REQUIRED, isolation = Isolation.READ_COMMITTED)
+    @Override
+    public Media update(Long id, Media media) {
+        String sql = "UPDATE media SET url = ?, media_type = ? WHERE id = ?";
+        int rowAffected = template.update(sql, media.getUrl(), media.getMediaType());
+        return rowAffected > 0 ? media : null;
+    }
+
+    @Transactional(propagation = Propagation.REQUIRED, isolation = Isolation.READ_COMMITTED)
+    @Override
+    public Media findById(Long id) {
+        try {
+            String sql = "SELECT id, url, media_type FROM media WHERE id = ?";
+            return template.queryForObject(sql, (rs, rowNum) -> {
+                Media media = new Media();
+                media.setId(rs.getLong("id"));
+                media.setUrl(rs.getString("url"));
+                media.setMediaType(MediaType.valueOf(rs.getString("media_type")));
+                return media;
+            },id);
+        } catch (DataAccessException e) {
+            throw new MediaNotFoundException("Media not found with a id: " + id);
+        }
+    }
+
+    @Transactional(propagation = Propagation.REQUIRED, isolation = Isolation.READ_COMMITTED)
+    @Override
+    public int deleteById(Long id) {
+        try {
+            String sql = "DELETE FROM media WHERE id = ?";
+            return template.update(sql, id);
+        } catch (DataAccessException e) {
+            throw new MediaNotFoundException("Media not found with a id: " + id);
+        }
+    }
+}
---
+++ src/main/java/com/app/DAO/MediaDao.java
@@ -0,0 +1,36 @@
+package com.app.DAO;
+
+import com.app.Model.Media;
+
+/**
+ * Data Access Object interface for managing Media entities.
+ */
+public interface MediaDao {
+    /**
+     * Save a new Media
+     * @param media The media to be saved.
+     * @return the saved media
+     */
+    Media save(Media media);
+
+    /**
+     * Update a existing media.
+     * @param id The ID of media to be updated
+     * @param media The media to be saved
+     * @return the updated media
+     */
+    Media update(Long id, Media media);
+    /**
+     * Find a media by its ID.
+     * @param id The ID of the media
+     * @return The media if found, or exception if not found.
+     */
+    Media findById(Long id);
+
+    /**
+     * Delete a media by its ID.
+     * @param id the ID of media
+     * @return The number of row affected.
+     */
+    int deleteById(Long id);
+}
