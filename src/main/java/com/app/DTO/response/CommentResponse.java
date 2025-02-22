3a28ca3d8e5f27035f99a53f9b05c93f253de8ac
Thien phuc <thienphuc484@gmail.com>
2025-02-22 11:30:13 +0700

    feat(CommentResponse): add created_at

--- src/main/java/com/app/DTO/response/CommentResponse.java
+++ src/main/java/com/app/DTO/response/CommentResponse.java
@@ -2,11 +2,14 @@ package com.app.DTO.response;
 
 import io.swagger.v3.oas.annotations.media.Schema;
 
+import java.time.LocalDateTime;
+
 @Schema(description = "Details of a single comment")
 public record CommentResponse(
         @Schema(description = "Id of the comment", example = "123") Long id,
         @Schema(description = "Content of the comment", example = "HELLO WORLD") String content,
         @Schema(description = "Id of the pin associated with comment", example = "123") long pinId,
         @Schema(description = "Id of the user associated with comment", example = "123") long userId,
-        @Schema(description = "Id of the media associated with comment", example = "123") long mediaId
+        @Schema(description = "Id of the media associated with comment", example = "123") long mediaId,
+        LocalDateTime created_at
 ) { }
