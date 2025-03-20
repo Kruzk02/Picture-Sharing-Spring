762e6e6b80ee75e29a1bd226b5ded5f3e775c10a
Thien phuc <thienphuc484@gmail.com>
2025-03-20 11:20:03 +0700

    feat(Controller): add NotificationController

---
+++ src/main/java/com/app/Controller/NotificationController.java
@@ -0,0 +1,41 @@
+package com.app.Controller;
+
+import com.app.Service.NotificationService;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RequestMapping("/api/notification")
+@AllArgsConstructor
+public class NotificationController {
+
+    private final NotificationService notificationService;
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteById(@PathVariable Long id) {
+        notificationService.deleteById(id);
+        return ResponseEntity.status(HttpStatus.NO_CONTENT)
+                .contentType(MediaType.APPLICATION_JSON)
+                .build();
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Void> markAsRead(@PathVariable Long id) {
+        notificationService.markAsRead(id);
+        return ResponseEntity.status(HttpStatus.OK)
+                .contentType(MediaType.APPLICATION_JSON)
+                .build();
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Void> markAllAsRead() {
+        notificationService.markAllAsRead();
+        return ResponseEntity.status(HttpStatus.OK)
+                .contentType(MediaType.APPLICATION_JSON)
+                .build();
+    }
+
+
+}
