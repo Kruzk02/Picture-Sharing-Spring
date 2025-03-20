e8d9ef26514c474c72451dc95085843de00ab271
Thien phuc <thienphuc484@gmail.com>
2025-03-20 12:34:04 +0700

    feat(KafkaConfig): add notification Producer and Consumer factory

--- src/main/java/com/app/Config/KafkaConfig.java
+++ src/main/java/com/app/Config/KafkaConfig.java
@@ -1,6 +1,9 @@
 package com.app.Config;
 
+import com.app.Model.Notification;
+import com.app.serde.deserializer.NotificationEventDeserializer;
 import com.app.serde.deserializer.VerificationEmailEventDeserializer;
+import com.app.serde.serializer.NotificationEventSerializer;
 import com.app.serde.serializer.VerificationEmailEventSerializer;
 import org.apache.kafka.clients.admin.AdminClientConfig;
 import org.apache.kafka.clients.admin.NewTopic;
@@ -35,6 +38,15 @@ public class KafkaConfig {
     @Value("${kafka.topic.email-event.replication-factor:1}")
     private short emailEventReplicationFactor;
 
+    @Value("${kafka.topic.notification-event.name}")
+    private String notificationEventTopicName;
+
+    @Value("${kafka.topic.notification-event.partitions:1}")
+    private int notificationEventPartitions;
+
+    @Value("${kafka.topic.notification-event.replication-factor:1}")
+    private short notificationEventReplicationFactor;
+
     @Bean
     public KafkaAdmin kafkaAdmin() {
         Map<String, Object> configs = new HashMap<>();
@@ -47,7 +59,7 @@ public class KafkaConfig {
         return new NewTopic(emailEventTopicName, emailEventPartitions, emailEventReplicationFactor);
     }
 
-    private ProducerFactory<String, Object> producerFactory() {
+    private ProducerFactory<String, Object> emailProducerFactory() {
         Map<String, Object> configs = new HashMap<>();
         configs.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapAddress);
         configs.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
@@ -57,15 +69,16 @@ public class KafkaConfig {
 
     @Bean
     public KafkaTemplate<String, Object> emailKafkaTemplate() {
-        KafkaTemplate<String, Object> template = new KafkaTemplate<>(producerFactory());
+        KafkaTemplate<String, Object> template = new KafkaTemplate<>(emailProducerFactory());
         template.setProducerListener(new CompositeProducerListener<>());
         return template;
     }
 
-    private ConsumerFactory<String, Object> consumerFactory(String groupId) {
+
+    private ConsumerFactory<String, Object> emailConsumerFactory() {
         Map<String, Object> configs = new HashMap<>();
         configs.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapAddress);
-        configs.put(ConsumerConfig.GROUP_ID_CONFIG, groupId);
+        configs.put(ConsumerConfig.GROUP_ID_CONFIG, "email-group");
         configs.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
         configs.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, VerificationEmailEventDeserializer.class);
         return new DefaultKafkaConsumerFactory<>(configs);
@@ -74,7 +87,44 @@ public class KafkaConfig {
     @Bean
     public ConcurrentKafkaListenerContainerFactory<String, Object> emailKafkaListenerContainerFactory() {
         ConcurrentKafkaListenerContainerFactory<String, Object> factory = new ConcurrentKafkaListenerContainerFactory<>();
-        factory.setConsumerFactory(consumerFactory("email-group"));
+        factory.setConsumerFactory(emailConsumerFactory());
+        factory.setConcurrency(3);
+        return factory;
+    }
+
+    @Bean
+    public NewTopic notificationEventTopic() {
+        return new NewTopic(notificationEventTopicName, notificationEventPartitions, notificationEventReplicationFactor);
+    }
+
+    private ProducerFactory<String, Object> notificationProducerFactory() {
+        Map<String, Object> configs = new HashMap<>();
+        configs.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapAddress);
+        configs.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        configs.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, NotificationEventSerializer.class);
+        return new DefaultKafkaProducerFactory<>(configs);
+    }
+
+    @Bean
+    public KafkaTemplate<String, Object> notificationKafkaTemplate() {
+        KafkaTemplate<String, Object> template = new KafkaTemplate<>(notificationProducerFactory());
+        template.setProducerListener(new CompositeProducerListener<>());
+        return template;
+    }
+
+    private ConsumerFactory<String, Notification> notificationConsumerFactory() {
+        Map<String, Object> configs = new HashMap<>();
+        configs.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapAddress);
+        configs.put(ConsumerConfig.GROUP_ID_CONFIG, "notification-group");
+        configs.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+        configs.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, NotificationEventDeserializer.class);
+        return new DefaultKafkaConsumerFactory<>(configs);
+    }
+
+    @Bean
+    public ConcurrentKafkaListenerContainerFactory<String, Notification> notificationKafkaListenerContainerFactory() {
+        ConcurrentKafkaListenerContainerFactory<String, Notification> factory = new ConcurrentKafkaListenerContainerFactory<>();
+        factory.setConsumerFactory(notificationConsumerFactory());
         factory.setConcurrency(3);
         return factory;
     }
