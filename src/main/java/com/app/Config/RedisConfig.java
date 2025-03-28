21d7cd953936a4352d70be7b2a71edced6907f6b
Thien phuc <thienphuc484@gmail.com>
2025-03-28 11:03:52 +0700

    delete: rate limit in spring boot

--- src/main/java/com/app/Config/RedisConfig.java
+++ src/main/java/com/app/Config/RedisConfig.java
@@ -4,17 +4,8 @@ import com.app.Model.Board;
 import com.app.Model.Comment;
 import com.app.Model.Pin;
 import com.app.Model.SubComment;
-import io.github.bucket4j.Bandwidth;
-import io.github.bucket4j.BucketConfiguration;
-import io.github.bucket4j.distributed.ExpirationAfterWriteStrategy;
-import io.github.bucket4j.distributed.proxy.ProxyManager;
-import io.github.bucket4j.redis.lettuce.cas.LettuceBasedProxyManager;
 import io.lettuce.core.RedisClient;
 import io.lettuce.core.RedisURI;
-import io.lettuce.core.api.StatefulRedisConnection;
-import io.lettuce.core.codec.ByteArrayCodec;
-import io.lettuce.core.codec.RedisCodec;
-import io.lettuce.core.codec.StringCodec;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -25,9 +16,6 @@ import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactor
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.JdkSerializationRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
-
-import java.time.Duration;
-import java.util.function.Supplier;
 
 @Configuration
 public class RedisConfig {
@@ -50,28 +38,6 @@ public class RedisConfig {
                 .withPort(redisPort)
                 .withSsl(false)
                 .build());
-    }
-
-    @Bean
-    public StatefulRedisConnection<String, byte[]> redisConnection(RedisClient redisClient) {
-        return redisClient.connect(RedisCodec.of(StringCodec.UTF8, ByteArrayCodec.INSTANCE));
-    }
-
-    @Bean
-    public ProxyManager<String> proxyManager(StatefulRedisConnection<String, byte[]> redisConnection) {
-        return LettuceBasedProxyManager.builderFor(redisConnection)
-                .withExpirationStrategy(ExpirationAfterWriteStrategy.basedOnTimeForRefillingBucketUpToMax(Duration.ofMinutes(1L)))
-                .build();
-    }
-
-    @Bean
-    public Supplier<BucketConfiguration> bucketConfiguration() {
-        return () -> BucketConfiguration.builder()
-                .addLimit(Bandwidth.builder()
-                        .capacity(50L)
-                        .refillGreedy(50L, Duration.ofMinutes(1L))
-                        .build())
-                .build();
     }
 
     @Bean
